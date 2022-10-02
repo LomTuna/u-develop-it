c46eaf087cace85c1874c605aa9d42e2669ecc60
TomLuna <lomtuna15@gmail.com>
2022-10-02 16:45:18 -0500

    added db.query commands to select data from tables

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const { application } = require('express');
 const express = require('express'); 
 const req = require('express/lib/request');
@@ -7,6 +8,22 @@ const app = express();
 //Express middleware 
 app.use(express.urlencoded({extended: false})); 
 app.use(express.json()); 
+
+//Connects to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    //mySQL Username
+    user: 'root',
+    password: 'Ballinger15', 
+    database: 'election'
+  },
+  console.log('Connected to the election database.')
+);
+
+db.query('SELECT * FROM candidates', (err, rows) =>{
+  console.log(rows);
+});
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
